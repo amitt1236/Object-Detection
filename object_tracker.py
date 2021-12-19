cdacf5c130cef345904ce903e3a202e186f99c00
amitt1236 <62837431+amitt1236@users.noreply.github.com>
2021-12-19 12:13:35 +0200

    colab

--- object_tracker.py
+++ object_tracker.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 import core.utils as utils
 
 from absl import app, flags, logging
